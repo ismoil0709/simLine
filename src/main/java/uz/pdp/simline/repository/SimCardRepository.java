19e4a18aff5d2a9b72b0f546348bbab41f46450d
eeslambec <eeslambec@gmail.com>
2024-01-20 00:56:56 +0500

    getAll and getAllByPlan methods created.

--- src/main/java/uz/pdp/simline/repository/SimCardRepository.java
+++ src/main/java/uz/pdp/simline/repository/SimCardRepository.java
@@ -3,8 +3,10 @@ package uz.pdp.simline.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import uz.pdp.simline.entity.Plan;
 import uz.pdp.simline.entity.SimCard;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,5 +17,6 @@ public interface SimCardRepository extends JpaRepository<SimCard, UUID> {
     Optional<SimCard> findSimCardByPriceBetweenMaxPriceAndMinPrice(Double minPrice, Double maxPrice);
 
     Optional<SimCard> findByIsActive(Boolean isActive);
-    Optional<SimCard> deleteByNumber(String number);
+
+    List<SimCard> findAllByPlan(Plan plan);
 }
--- src/main/java/uz/pdp/simline/service/impl/SimCardServiceImpl.java
+++ src/main/java/uz/pdp/simline/service/impl/SimCardServiceImpl.java
@@ -3,26 +3,21 @@ package uz.pdp.simline.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.pdp.simline.dto.SimCardUpdateDto;
+import uz.pdp.simline.entity.Plan;
 import uz.pdp.simline.entity.SimCard;
 import uz.pdp.simline.exception.NotFoundException;
 import uz.pdp.simline.exception.NullOrEmptyException;
 import uz.pdp.simline.repository.SimCardRepository;
 import uz.pdp.simline.service.SimCardService;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class SimCardServiceImpl implements SimCardService {
     private final SimCardRepository simCardRepository;
-    @Override
-    public SimCard create(SimCard simCard) {
-        if (simCard.getNumber() == null || simCard.getNumber().isEmpty() || simCard.getNumber().isBlank())
-            throw new NullOrEmptyException("Number is empty!");
-        if (simCard.getPrice() == null)
-            throw new NullOrEmptyException("Price is empty!");
-        return simCardRepository.save(simCard);
-    }
+
 
     @Override
     public SimCard getById(UUID id) {
@@ -63,21 +58,14 @@ public class SimCardServiceImpl implements SimCardService {
     }
 
     @Override
-    public void deleteById(UUID id) {
-        if (id == null)
-            throw new NullOrEmptyException("Id is empty!");
-        simCardRepository.deleteById(id);
+    public List<SimCard> getAllByPlan(Plan plan) {
+        if (plan == null)
+            throw new NullOrEmptyException("Plan is empty!");
+        return simCardRepository.findAllByPlan(plan);
     }
 
     @Override
-    public void deleteByNumber(String number) {
-        if (number == null || number.isEmpty() || number.isBlank())
-            throw new NullOrEmptyException("Number is empty!");
-        simCardRepository.deleteByNumber(number);
-    }
-
-    @Override
-    public void deleteByActivity(Boolean isActive) {
-        // Check it. If it's possible I'll finish it up
+    public List<SimCard> getAll() {
+        return simCardRepository.findAll();
     }
 }
